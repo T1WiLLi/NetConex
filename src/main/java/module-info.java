b89a842cebf582a24753e148d76337ef83ffacbe
T1WiLLi <williambeaudun@gmail.com>
2024-06-06 00:42:32 -0400

    Fixed the pom.xml

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module netconex {
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+
+    exports qc.netconex;
+}
--- src/main/java/qc/netconex/Main.java
+++ src/main/java/qc/netconex/Main.java
@@ -1,12 +1,7 @@
 package qc.netconex;
 
-import qc.netconex.model.Student;
-import qc.netconex.request.Get;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Get getRequest = new Get(new HttpRequester("https://cms-api-1crm.onrender.com/api/v1/"));
-        Student student = getRequest.executeAndDeserialize("students/1", Student.class);
-        System.out.println(student);
+
     }
 }
